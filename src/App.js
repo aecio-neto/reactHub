1af36cea842ac6c6ff1b35839d458b0494552482
Aécio Neto <aecioneto@MacBook-Pro-de-Aecio-2.local>
2023-09-27 10:56:38 -0300

    add table formating for project links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import UserProfile from "./projetos/projeto1_userProfile/UserProfile"
 import StepsCard from "./projetos/projeto2_stepsCard/StepsCard"
 import FutureDate from "./projetos/projeto3_futureDate/FutureDate"
@@ -12,50 +7,67 @@ import Accordion from "./projetos/projeto5_accordion/Accordion"
 import TipCalculator from "./projetos/projeto6_tipCalculator/TipCalculator"
 import PizzaMenu from "./projetos/projeto_pizzaMenu/PizzaMenu"
 import TravelList from "./projetos/projeto_travelList/components/TravelList"
+import RachaConta from "./projetos/projeto7_rachaConta/RachaConta"
 import "./global.css"
 
 function App() {
   return (
     <Router>
       <div className="projects-container">
-        <nav>
+        <h1>Projetos React</h1>
+        <div className="table-container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Júnior</th>
+                <th>Intermediário</th>
+                <th>Avançado</th>
+                <th>Profissional</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <Link to="/projeto7">Racha Conta</Link>
+                  <br />
+                  <Link to="/projeto6">Calculadora de Gorjetas</Link>
+                  <br />
+                  <Link to="/projeto5">Acordeão</Link>
+                  <br />
+                  <Link to="/projeto4">Flash Cards</Link>
+                  <br />
+                  <Link to="/projeto3">Data Futura</Link>
+                  <br />
+                  <Link to="/projeto2">Steps Card</Link>
+                  <br />
+                  <Link to="/projeto1">Cartão de Perfil de Usuário</Link>
+                  <br />
+                  <Link to="/pizza-menu">Pizza Menu</Link>
+                  <br />
+                  <Link to="/baggage-buddy">Baggage Buddy</Link>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
           <ul>
-            <li>
-              <Link to="/projeto6">Calculadora de Gorjetas</Link>
-            </li>
-            <li>
-              <Link to="/projeto5">Acordeão</Link>
-            </li>
-            <li>
-              <Link to="/projeto4">Flash Cards</Link>
-            </li>
-            <li>
-              <Link to="/projeto3">Data Futura</Link>
-            </li>
-            <li>
-              <Link to="/projeto2">Steps Card</Link>
-            </li>
-            <li>
-              <Link to="/projeto1">Cartão de Perfil de Usuário</Link>
-            </li>
-            <li>
-              <Link to="/pizza-menu">Pizza Menu</Link>
-            </li>
-            <li>
-              <Link to="/baggage-buddy">Baggage Budy</Link>
-            </li>
+            <li><strong>Júnior: </strong> foco em gerenciamento de estado</li>
           </ul>
-        </nav>
-          <Routes>
-            <Route path="/projeto6" element={<TipCalculator />} />
-            <Route path="/projeto5" element={<Accordion />} />
-            <Route path="/projeto4" element={<FlashCards />} />
-            <Route path="/projeto3" element={<FutureDate />} />
-            <Route path="/projeto2" element={<StepsCard />} />
-            <Route path="/projeto1" element={<UserProfile />} />
-            <Route path="/pizza-menu" element={<PizzaMenu />} />
-            <Route path="/baggage-buddy" element={<TravelList />} />
-          </Routes>
+          <p>Abaixo você poderá ver a renderização dos projetos. Os estilos ainda estão sendo configurados. A visualização será melhor no desktop.</p>
+        </div>
+        <Routes>
+          <Route path="/projeto7" element={<RachaConta />} />
+          <Route path="/projeto6" element={<TipCalculator />} />
+          <Route path="/projeto5" element={<Accordion />} />
+          <Route path="/projeto4" element={<FlashCards />} />
+          <Route path="/projeto3" element={<FutureDate />} />
+          <Route path="/projeto2" element={<StepsCard />} />
+          <Route path="/projeto1" element={<UserProfile />} />
+          <Route path="/pizza-menu" element={<PizzaMenu />} />
+          <Route path="/baggage-buddy" element={<TravelList />} />
+        </Routes>
       </div>
     </Router>
   )
